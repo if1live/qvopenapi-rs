b8033a37c28d44770503b7a8313b5f00f267c833
if1live <libsora25@gmail.com>
2024-09-15 13:46:13 +0900

    s8064 삭제
    1. 거래비밀번호 항목과 싸우다 포기
    2. HTS에서 "8220 일별 합산 잔고" 쓰면 해외, 펀드까지 읽을수 있어서

--- qvopenapi-http/src/routes/query.rs
+++ qvopenapi-http/src/routes/query.rs
@@ -75,36 +75,3 @@ async fn query_c8010(
 
     Ok(reply::with_status(reply::json(&result), StatusCode::OK))
 }
-
-pub fn filter_s8064(client: Arc<QvOpenApiAsyncClient>) -> BoxedFilter<(impl Reply,)> {
-    let cloned = client.clone();
-    let handler = move |req: S8064Request| query_s8064(cloned.clone(), req);
-    post()
-        .and(warp::path!("query" / "s8064"))
-        .and(body::json())
-        .and_then(handler)
-        .boxed()
-}
-
-async fn query_s8064(
-    client: Arc<QvOpenApiAsyncClient>,
-    request: S8064Request,
-) -> Result<impl Reply, Infallible> {
-    let ret = client.query(request.into_raw()).await;
-
-    if ret.is_err() {
-        return error::convert_error(ret.err().unwrap());
-    }
-
-    let result = ret.unwrap();
-
-    let error = result.get("error");
-    if error.is_some() && !error.unwrap().is_null() {
-        return Ok(reply::with_status(
-            reply::json(&result),
-            StatusCode::INTERNAL_SERVER_ERROR,
-        ));
-    }
-
-    Ok(reply::with_status(reply::json(&result), StatusCode::OK))
-}
--- qvopenapi/src/models/query/mod.rs
+++ qvopenapi/src/models/query/mod.rs
@@ -1,10 +1,8 @@
 mod c8010;
 mod c8201;
-mod s8064;
 pub use c8010::*;
 pub use c8201::*;
 use qvopenapi_bindings::OutDataBlock;
-pub use s8064::*;
 use serde::Serialize;
 
 use serde_json::Value;
@@ -54,11 +52,9 @@ fn parse_block(
     block_len: i32,
 ) -> Result<Value, QvOpenApiError> {
     match block_name {
+        BLOCK_NAME_C8010_OUT => parse_c8010_response(block_data, block_len),
         BLOCK_NAME_C8201_OUT => parse_c8201_response(block_data, block_len),
         BLOCK_NAME_C8201_OUT1_ARRAY => parse_c8201_response1_array(block_data, block_len),
-        BLOCK_NAME_C8010_OUT => parse_c8010_response(block_data, block_len),
-        BLOCK_NAME_S8064_OUT => parse_s8064_response(block_data, block_len),
-        BLOCK_NAME_S8064_OUT1_ARRAY => parse_s8064_response1_array(block_data, block_len),
         _ => Err(QvOpenApiError::UnimplementedBlockError {
             block_name: block_name.into(),
         }),
